5cbb2c0f2e00735c3154908fc02fa614f89bf51e
Umera <umeramalek796@gmail.com>
2021-11-01 00:48:08 -0700

    added relationships to the models

---
+++ models/index.js
@@ -0,0 +1,45 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// relationships
+// user can have many post
+// user can have many comments 
+// a post can have many comments
+// post has a user
+// commment are post
+// comments are users 
+
+
+User.hasMany(Post, {
+    foreignKey:'user_id',
+    onDelete: 'CASCADE',
+});
+
+User.hasMany(Comment, {
+    foreignKey:'user_id',
+    onDelete: 'CASCADE',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+module.exports = {User, Post, Comment};
+
