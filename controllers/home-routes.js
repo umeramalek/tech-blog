49d4554b38f5eb6b39f458cb69466cfa370efe09
Umera <umeramalek796@gmail.com>
2021-11-01 19:31:20 -0700

    gte a single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,11 +12,15 @@ const withAuth = require('../utils/auth');
 router.get('/', async(req,res) => {
     try {
          // Get all projects and JOIN with user data
-        const getData = await Post.findAll({
-            include: [{model: User, attributes: ['username']}],
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User, 
+                    attributes: ['username']
+                }],
         });
         // Serialize data so the template can read it
-        const data = getData.map((post) => post.get({plain:true}));
+        const data = postData.map((post) => post.get({plain:true}));
 
         // Pass serialized data and session flag into template
         res.render('homepage', { 
@@ -26,6 +30,38 @@ router.get('/', async(req,res) => {
         console.log(err)
         res.status(500).json(err);
     }
-})
+});
+
+
+// Get single post
+router.get('/project/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [
+        {
+            model: User,
+            attributes: ['username'],
+        },
+        {
+            model: Comment,
+            attribute: ['id','content','post_id','user_id'],
+            include: [{
+                model: User,
+                attributes: ['id','username']
+            }]
+        }
+        ],
+      });
+  
+      const post = postData.get({ plain: true });
+  
+      res.render('post', {post, logged_in: req.session.logged_in});
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  
+
 
 module.exports = router;
