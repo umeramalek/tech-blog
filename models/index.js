912c860640d4c957e8788090950bca1b748828b5
Umera <umeramalek796@gmail.com>
2021-11-01 22:53:55 -0700

    bug fixes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./user');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const Post = require('./post');
+const Comment = require('./comment');
 
 // relationships
 // user can have many post
