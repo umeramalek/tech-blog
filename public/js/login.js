fcf239d943ed187322461924663c098bac0ee068
Umera <umeramalek796@gmail.com>
2021-11-01 20:56:05 -0700

    added a sign up form

--- public/js/login.js
+++ public/js/login.js
@@ -23,6 +23,30 @@ const loginSection = async (event) => {
     }
 };
 
+const signUpSection = async (event) => {
+    event.preventDefault();
 
+    const username = document.querySelector("#name-signup").value.trim();
+    const email = document.querySelector("#email-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                email, username, password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            document.location.replace('/');
+        }
+        else {
+            alert('Failed to signup');
+        }
+    }
+};
 
 document.querySelector("#login-form").addEventListener('submit', loginSection);
+document.querySelector('.signup-form').addEventListener('submit', signUpSection);
