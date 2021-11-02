1ca18eae1e49224861d43feec87c1b0ba688e13c
Umera <umeramalek796@gmail.com>
2021-11-01 21:01:04 -0700

    added a logout section

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+const logoutSection = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+    if (response.ok) {
+        document.location.replace('/login');
+    }
+    else {
+        alert('Failed to logout');
+    }
+};
+
+document.querySelector("#logout").addEventListener('click', logoutSection);
